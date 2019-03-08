1784c821f43fe1fc42f4d97b88d74b980124e0e5
AzureAD\EricYew <eric.yew@ecenta.com>
2019-03-09 04:30:10 +0800

    added firebase

--- js/function.js
+++ js/function.js
@@ -1,3 +1,13 @@
 function navi(filename){
     window.location.href = "/ncloyaltyprogram/" + filename + ".html";
 }
+
+
+//firebase
+function pushFBase(){
+    var firebaseRef = firebase.database().ref();
+
+    firebaseRef.push().set({
+        points: 10
+    });
+}
